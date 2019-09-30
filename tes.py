4ee2f741d9d5c85ab31fe26e1e06f7adf17d0d59
lintangwisesa <lintangwisesa@ymail.com>
2019-09-30 15:27:39 +0700

    opencv

---
+++ tes.py
@@ -0,0 +1,40 @@
+gambar = 'download.jpg'
+
+# Face Detection using Cascade Classifier
+
+import cv2
+
+# https://github.com/opencv/opencv/tree/master/data/haarcascades
+face_cascade = cv2.CascadeClassifier("./methods/haarcascades/haarcascade_frontalface_default.xml")
+
+img = cv2.imread(gambar)
+
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+wajah = face_cascade.detectMultiScale(
+    gray_img,
+    scaleFactor = 1.5,
+    minNeighbors = 5
+)
+
+print(wajah)
+print(type(wajah))
+
+# ===========================================
+# menambahkan kotak deteksi di area wajah
+# ===========================================
+
+for x, y, w, h in wajah:
+    img = cv2.rectangle(
+        img,            # image object
+        (x,y),          # posisi kotak
+        (x+w, y+h),     # posisi kotak
+        (0, 255, 0),    # warna kotak RGB
+        3               # lebar garis kotak
+    )
+
+resized = cv2.resize(img, (800,600))
+cv2.imshow('Lionel Messi', resized)
+# cv2.imwrite('5_outMessi.jpg', resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
